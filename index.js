2a0db81caa3d5c977c8f72ff7adbffdd45a73989
alldebaro@gmail.com <alldebaro@gmail.com>
2024-11-19 16:22:46 +0900

    1119화_영어로번역

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const TextTranslationClient = require("@azure-rest/ai-translation-text").default
+require('dotenv').config(); // .env 파일 로드
+const TextTranslationClient = require("@azure-rest/ai-translation-text").default;
 
-const apiKey = "8jI2YzhUrY3yLDTTrFwF1Bwow1tRrME7QYl66UwZHIipoq0pZojuJQQJ99AKACNns7RXJ3w3AAAbACOGew1E";
-const endpoint = "https://api.cognitive.microsofttranslator.com/";
-const region = "koreacentral";
+const apiKey = process.env.API_KEY;
+const endpoint = process.env.API_ENDPOINT;
+const region = process.env.API_REGION;
 
 async function main() {
-
   const translateCredential = {
     key: apiKey,
     region,
   };
-  const translationClient = new TextTranslationClient(endpoint,translateCredential);
+  // AzureKeyCredential 객체 생성
+  // const credential = new AzureKeyCredential(apiKey);
+
+  // 클라이언트 생성
+  const translationClient = new TextTranslationClient(endpoint, translateCredential);
 
   const inputText = [{ text: "저는 서울에 살아요" }];
   const translateResponse = await translationClient.path("/translate").post({
@@ -20,18 +24,16 @@ async function main() {
       from: "ko",
     },
   });
-
   const translations = translateResponse.body;
   for (const translation of translations) {
-    console.log(
-      `en_text : '${translation?.translations[0]?.text}'.`
-    );
+    console.log(`en_text : '${translation?.translations[0]?.text}'.`);
   }
 }
 
 main().catch((err) => {
-    console.error("An error occurred:", err);
-    process.exit(1);
-  });
+  console.error("An error occurred:", err);
+  process.exit(1);
+  
+});
 
-  module.exports = { main };
+module.exports = { main };
